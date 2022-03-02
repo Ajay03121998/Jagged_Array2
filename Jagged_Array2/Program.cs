72994aab7c883810c358d02cf621fc00ad78d6b5
Muppidi-Ajay-Kumar <Muppidi.Ajay.Kumar@ibm.com>
2022-03-02 11:51:40 +0530

    Add project files.

---
+++ Jagged_Array2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jagged_Array2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jag_arr = new int[][] { new int[] { 11, 22, 33, 44 },
+            new int[] { 53, 4, 69 },new int[] { 8, 2 },new int[] { 0, 4, 7, 3, 99 } };
+
+            for (int i = 0; i < jag_arr.Length; i++)
+            {
+                // Print the row number
+                Console.Write("Row({0}): ", i);
+                for (int k = 0; k < jag_arr[i].Length; k++)
+                {
+                    // Print the elements in the row
+                    Console.Write("{0} ", jag_arr[i][k]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
